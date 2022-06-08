0758918977e9d07dee0bfd733da20556437cae53
KD Botz <98203719+x-kunal@users.noreply.github.com>
2022-06-08 08:27:54 +0530

    info.py

---
+++ info.py
@@ -0,0 +1,18 @@
+import os
+
+API_ID=int(os.environ.get("API_ID"))
+API_HASH=os.environ.get("API_HASH")
+BOT_TOKEN=os.environ.get("BOT_TOKEN")
+SESSION=os.environ.get("SESSION")
+
+GROUPS = []
+for grp in (os.environ.get("GROUPS").split()):
+    GROUPS.append(int(grp))
+
+AUTO_DELETE = os.environ.get("AUTO_DELETE")
+DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL"))
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK")
+
+# remove or add more as per your need
+LIST = ["srt", "txt", "jpg", "jpeg", "png", "torrent", "html", "aio", "pdf", "zip"]
