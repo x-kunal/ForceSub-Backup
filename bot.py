b3733b2014b70c70a6409adca3958e3ef512b785
KD Botz <98203719+x-kunal@users.noreply.github.com>
2022-06-08 08:23:52 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,62 @@
+# KD Botz 
+
+# January 15th 2022
+
+from pyrogram import Client, filters, idle
+from helpers import copy_msg, force_sub, check_fsub, auto_delete
+from info import API_ID, API_HASH, BOT_TOKEN, SESSION, GROUPS, AUTO_DELETE
+
+Bot = Client(session_name="backup-fsub", 
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=300)
+# 🥲
+User = Client(session_name=SESSION, 
+              api_id=API_ID,
+              api_hash=API_HASH,
+              workers=300)
+ 
+@Bot.on_message(filters.private & filters.command("start"))
+async def start(bot, message):
+    await message.reply(f"<b>𝖧ᴇʟʟᴏ {message.from_user.mention} !</b>\n\n<b>Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ : @KD_Botz 😈</b>",
+                        disable_web_page_preview=True)
+
+@Bot.on_message(filters.chat(GROUPS))
+async def fsub(bot, message):
+    try:
+       await force_sub(bot, message)       
+    except Exception as e:
+       print(e)
+
+@User.on_message(filters.chat(GROUPS) & ~filters.edited) 
+async def user_bot(user, message):
+    if message.document or message.video:      
+        try:
+           await copy_msg(message)
+        except Exception as e:
+           print(e) 
+    if AUTO_DELETE == "True":
+        try:
+            await auto_delete(Bot, message)
+        except Exception as e:
+            print(e) 
+      
+@Bot.on_callback_query(filters.regex("checksub"))  
+async def try_again(bot, update):
+    try:
+       await check_fsub(bot, update)
+    except Exception as e:
+           print(e) 
+   
+User.start()
+print("Userbot Started!")
+Bot.start()
+print("Bot Started!")
+
+idle()
+
+User.stop()
+print("Userbot Stopped!")
+Bot.stop()
+print("Bot Stopped!")
